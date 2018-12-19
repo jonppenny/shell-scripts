446202b1bc699e378f5224bc5df4befa515372dc
Jonathan Penny <jonppenny@users.noreply.github.com>
2018-12-19 09:33:44 +0000

    Added script for Google Cloud storage
    Added script for Google Cloud storage. Pretty much the same as the AWS script.

---
+++ google_cloud_backup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DATAFILE=~/path/to/credentials
+
+read -r USER PASSWORD < "$DATAFILE"
+
+OUTPUT="/path/to/backup/directory"
+
+databases=`mysql --user=$USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+
+for db in $databases; do
+    if [[ "$db" == "carrick_intranet" ]] ; then
+	echo "Dumping database: $db"
+	mysqldump --force --opt --user=$USER --password=$PASSWORD --databases $db > $OUTPUT/`date +%Y%m%d%H%M%S`.$db.sql
+	gzip --force $OUTPUT/`date +%Y%m%d%H%M%S`.$db.sql
+	rm -rf $OUTPUT/*.sql
+	gsutil rsync -r $OUTPUT gs://google-bucket-name
+	rm -rf $OUTPUT/*.gz
+    fi
+done
