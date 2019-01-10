e818fc9eafe02def163bc6364d4b664b2e270110
Jonathan Penny <jonppenny@users.noreply.github.com>
2019-01-10 09:32:10 +0000

    Update google_cloud_backup.sh

--- google_cloud_backup.sh
+++ google_cloud_backup.sh
@@ -9,7 +9,7 @@ OUTPUT="/path/to/backup/directory"
 databases=`mysql --user=$USER --password=$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
-    if [[ "$db" == "carrick_intranet" ]] ; then
+    if [[ "$db" == "db_name" ]] ; then
 	echo "Dumping database: $db"
 	mysqldump --force --opt --user=$USER --password=$PASSWORD --databases $db > $OUTPUT/`date +%Y%m%d%H%M%S`.$db.sql
 	gzip --force $OUTPUT/`date +%Y%m%d%H%M%S`.$db.sql
